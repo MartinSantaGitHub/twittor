baeee63b3f165b7116ae63c137bd3e2024b502ca
martins <santasan27@gmail.com>
2022-11-09 20:05:03 -0300

    Environment variables

--- helpers/getEnv.go
+++ helpers/getEnv.go
@@ -1,17 +1,26 @@
 package helpers
 
 import (
+	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 /* GetEnvVariable Load the .env file and returns the value of the key */
 func GetEnvVariable(key string) string {
-	// load .env file
-	// err := godotenv.Load()
 
-	// if err != nil {
-	// 	log.Fatal("Error loading .env file")
-	// }
+	// If it's Dev environment, load the .env file
+	prod := os.Getenv("PROD")
+
+	if prod != "true" {
+		// load .env file
+		err := godotenv.Load()
+
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+	}
 
 	return os.Getenv(key)
 }
